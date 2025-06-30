38e6745ee504270ab293749c8cedd67d501c60f9
Clément T <52360173+caimez323@users.noreply.github.com>
2025-06-30 16:08:03 +0200

    Create Luxtro.lua

---
+++ jokers/Luxtro.lua
@@ -0,0 +1,33 @@
+SMODS.Joker {
+	key = 'Luxtro',
+	loc_txt = {
+		name = 'Luxtro',
+		text = {
+			"Gains +#1# Chips",
+		}
+	},
+	config = {
+		extra = {
+			slots = 1,
+		},
+		immutable = {
+			max_slots = 50,
+		},
+	},
+	rarity = 2,
+	atlas = 'Jokers',
+	pos = { x = 0, y = 0 },
+	cost = 5,
+	loc_vars = function(self, info_queue, center)
+		return { vars = { number_format(card.ability.extra.slots * G.GAME.pharahos_scarabs) } }
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		if (card.ability.extra.slots * G.GAME.pharahos_scarabs) > card.ability.immutable.max_slots then
+			(card.ability.extra.slots * G.GAME.pharahos_scarabs) = card.ability.immutable.max_slots
+		end
+		G.jokers.config.card_limit = lenient_bignum(G.jokers.config.card_limit + to_big((card.ability.extra.slots * G.GAME.pharahos_scarabs)))
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		G.jokers.config.card_limit = lenient_bignum(G.jokers.config.card_limit - to_big((card.ability.extra.slots * G.GAME.pharahos_scarabs)))
+	end,
+}
