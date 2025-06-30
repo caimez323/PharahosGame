dbff1a1f865a0a8d2498f1a0ab3ddd0f53179d93
Clément T <52360173+caimez323@users.noreply.github.com>
2025-06-30 14:57:59 +0200

    Update Hugo.lua

--- jokers/Hugo.lua
+++ jokers/Hugo.lua
@@ -1,25 +1,29 @@
 SMODS.Joker {
-	key = 'Hugo',
+    key = "Hugo",
 	loc_txt = {
 		name = 'Hugo',
 		text = {
-			"Gains {C:chips}+#1#{} Chips",
+			"Gains {C:chips}+#1#{} Chips per joker",
 		}
 	},
-	config = { extra = { chips = 0 } },
-	rarity = 2,
-	atlas = 'Jokers',
-	pos = { x = 0, y = 0 },
-	cost = 5,
-	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.chips } }
-	end,
-	calculate = function(self, card, context)
-		if context.joker_main then
-			return {
-				chip_mod = card.ability.extra.chips * G.GAME.pharahos_scarabs,
-				message = localize { type = 'variable', key = 'a_chips', vars = { card.ability.extra.chips * G.GAME.pharahos_scarabs } }
-			}
-		end
-	end
+    blueprint_compat = true,
+    rarity = 1,
+    cost = 4,
+    pos = { x = 0, y = 0 },
+    config = { extra = { chips = 25 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.chips, card.ability.extra.chips * (G.jokers and #G.jokers.cards or 0) * G.GAME.pharahos_scarabs} }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            -- THX to vanillaRemade : 
+            local joker_count = 0
+            for i = 1, #G.jokers.cards do
+                 if G.jokers.cards[i].ability.set == 'Joker' then joker_count = joker_count + 1 end
+            end
+            return {
+                chips = card.ability.extra.chips * joker_count * G.GAME.pharahos_scarabs
+            }
+        end
+    end,
 }
