51ff0740d6c9afd19d3e30d2f815ad3f7e9113c3
Clément T <52360173+caimez323@users.noreply.github.com>
2025-06-30 16:40:25 +0200

    Create scarabHand.lua

---
+++ jokers/scarabHand.lua
@@ -0,0 +1,18 @@
+SMODS.Joker {
+    key = "scarabHand",
+    blueprint_compat = false,
+    rarity = 1,
+    cost = 4,
+    pos = { x = 0, y = 0 },
+    config = { extra = { h_size = 1 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.h_size * G.GAME.pharahos_scarabs } }
+    end,
+    add_to_deck = function(self, card, from_debuff)
+        G.hand:change_size(card.ability.extra.h_size * G.GAME.pharahos_scarabs)
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+        G.hand:change_size(-card.ability.extra.h_size * G.GAME.pharahos_scarabs)
+    end
+}
+
