8bcdcc2f4d1ca0e88712fc72c99d64273998d06e
caimez323 <caimezae@gmail.com>
2025-07-04 00:19:25 +0200

    fix saytey

--- jokers/SayTey.lua
+++ jokers/SayTey.lua
@@ -4,7 +4,7 @@ SMODS.Joker {
 		name = 'SayTey',
 		text = {
 			"Rettriger {C:mult}+#1#{} fois",
-			"Le plus à droite"
+			"joker à droite"
 		}
 	},
 	config = {
@@ -20,22 +20,19 @@ SMODS.Joker {
 		return { vars = { math.min(center.ability.immutable.max_retriggers, center.ability.extra.retriggers * G.GAME.pharahos_scarabs) } }
 	end,
 	calculate = function(self, card, context)
+		local my_pos = nil
+        for i = 1, #G.jokers.cards do
+            if G.jokers.cards[i] == card then my_pos = i; break end
+        end
 		if context.retrigger_joker_check then
-			print(context.retrigger_joker_check, context.retrigger_joker, context.other_card ~= self)
-			if context.retrigger_joker_check and not context.retrigger_joker and context.other_card ~= self then
-				print("ici")
-				if context.other_card == G.jokers.cards[1] then
-					print("ici 2")
-					return {
-						message = localize("k_again_ex"),
-						repetitions = to_number(
-							math.min(card.ability.immutable.max_retriggers, card.ability.extra.retriggers * G.GAME.pharahos_scarabs)
-						),
-						card = card,
-					}
-				else
-					return nil, true
-				end
+			if my_pos and G.jokers.cards[my_pos + 1] and context.other_card == G.jokers.cards[my_pos + 1] then
+				return {
+					message = localize("k_again_ex"),
+					repetitions = to_number(
+						math.min(card.ability.immutable.max_retriggers, card.ability.extra.retriggers * G.GAME.pharahos_scarabs)
+					),
+					card = card,
+				}
 			end
 		end
 	end,
