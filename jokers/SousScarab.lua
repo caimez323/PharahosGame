4098a16bde796d335cd414761fc8162397ee00f0
Clément T <52360173+caimez323@users.noreply.github.com>
2025-07-04 14:36:36 +0200

    Create SousScarab.lua

---
+++ jokers/SousScarab.lua
@@ -0,0 +1,30 @@
+SMODS.Joker {
+    key = "SousScarab",
+    loc_txt = {
+        name = 'SousScarab',
+        text = {
+            "1 ou 6"
+        }
+    },
+    blueprint_compat = true,
+    pos = { x = 2, y = 2 },
+    rarity = 3,
+    cost = 10,
+    atlas = "Jokers",
+    config = { extra = { dollar = 1, dollar_gain = 5 } },
+    loc_vars = function(self, info_queue, center)
+        return {
+            vars = {
+                number_format(center.ability.extra.dollar),
+                number_format(center.ability.extra.dollar_gain),
+            },
+        }
+    end,
+    calc_dollar_bonus = function(self, card)
+        local bonus = card.ability.extra.dollar
+        if G.GAME.pharahos_scarabs >= 1 then
+                gain = gain + card.ability.extra.dollar_gain
+        end
+        return bonus
+    end
+}
